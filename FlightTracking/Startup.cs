72fb198407aa69739993ae4f2a2d284661aad760
eslam khaled <eslam.khaled1002@gmail.com>
2019-11-10 12:57:51 +0200

    First commit

---
+++ FlightTracking/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FlightTracking.Startup))]
+namespace FlightTracking
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
