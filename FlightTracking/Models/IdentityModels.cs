5835b04b4f7266c6a1130198321fc29d7d572205
kholoudsalah <kholoud.salahhh@gmail.com>
2019-11-11 16:30:03 +0200

    khloud edite to wep config and identidy model class

--- FlightTracking/Models/IdentityModels.cs
+++ FlightTracking/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace FlightTracking.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("FlightTrackingConnection", throwIfV1Schema: false)
         {
         }
 
