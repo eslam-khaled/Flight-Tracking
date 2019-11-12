104084dac263fd4a3f1a03165a1a87cdde8359af
kholoudsalah <kholoud.salahhh@gmail.com>
2019-11-12 15:15:53 +0200

    add and delete-plane controller

--- FlightTracking/Controllers/PlaneController.cs
+++ FlightTracking/Controllers/PlaneController.cs
@@ -3,15 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using FlightTracking.Models;
 namespace FlightTracking.Controllers
 {
+
     public class PlaneController : Controller
     {
+        ApplicationDbContext context;
+
+        public PlaneController(){
+
+            context = new ApplicationDbContext(); 
+
+        }
+
+        
         // GET: Plane
         public ActionResult Index()
         {
             return View();
         }
+        public ActionResult create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult create( Plane plane)
+        {
+            if (plane == null)
+            {
+                
+            }
+            context.planes.Add(plane);
+            context.SaveChanges();
+            return View("create");
+        }
+        public ActionResult delete(int id)
+        {
+            var record = context.planes.Find(id);
+            context.planes.Remove(record);
+            context.SaveChanges();
+            return View();
+        }
+
     }
 }
