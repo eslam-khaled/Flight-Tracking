4dd3ffe3c248e92fa81b91d32ad864d71ad44326
Abdulrahman Ashraf <abdulrahman.ashraf246@gmail.com>
2019-11-12 16:30:39 +0200

    Edit and Delete passanger

--- FlightTracking/Controllers/PassangerController.cs
+++ FlightTracking/Controllers/PassangerController.cs
@@ -3,15 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FlightTracking.Models;
 
 namespace FlightTracking.Controllers
 {
     public class PassangerController : Controller
     {
+        ApplicationDbContext context;
+        public PassangerController()
+        {
+            context = new ApplicationDbContext();
+        }
         // GET: Passanger
         public ActionResult Index()
         {
             return View();
+        }
+
+        //DeletePassanger
+        public ActionResult DeletePassanger(int? id)
+        {
+            Passanger passanger = context.passangers.Find(id);
+            var ResDetails = context.passangers.Where(x => x.Id == id).FirstOrDefault();
+            return View(ResDetails);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeletePassanger(Passanger passanger, int id)
+        {
+            var MyDel = context.passangers.Where(c => c.Id == id).FirstOrDefault();
+            context.passangers.Remove(MyDel);
+            context.SaveChanges();
+
+            return View("Index");
+        }
+
+        //EditPassanger
+        public ActionResult EditPassanger(int? id)
+        {
+            Passanger passanger = context.passangers.Find(id);
+            var ResDetails = context.passangers.Where(x => x.Id == id).FirstOrDefault();
+            return View(ResDetails);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditPassanger(Passanger passanger, int id)
+        {
+            var MyEdit = context.passangers.Where(c => c.Id == id).FirstOrDefault();
+            MyEdit.Name = passanger.Name;
+            MyEdit.Nationality =passanger.Nationality;
+            context.SaveChanges();
+
+            return View("EditPassanger");
         }
     }
 }
