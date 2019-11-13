f399eae221a586931677a390e2f30b3d3df4270b
eslam khaled <eslam.khaled1002@gmail.com>
2019-11-13 14:10:48 +0200

    Re-modeling2

---
+++ FlightTracking/Migrations/201911131209576_Re-modeling.cs
@@ -0,0 +1,16 @@
+namespace FlightTracking.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Remodeling : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
